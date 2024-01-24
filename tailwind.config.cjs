cdc2e7ea81abe837a6456208a35278ddcd2451a9
tbrooke <tom.brooke@gmail.com>
2024-01-23 20:49:32 -0500

    update header, footer, index

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -18,6 +18,7 @@ module.exports = {
       },
     },
   },
-  plugins: [require('@tailwindcss/typography')],
+  plugins: [require('@tailwindcss/typography'),
+            require("daisyui")],
   darkMode: 'class',
 };
