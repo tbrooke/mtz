723e6b77b396d469644590f688f130f87eadeba9
tbrooke <tom.brooke@gmail.com>
2024-05-13 20:15:32 -0400

    added storyblok component

--- astro.config.mjs
+++ astro.config.mjs
@@ -15,6 +15,7 @@ import tasks from './src/utils/tasks';
 import { defineConfig } from "astro/config";
 import storyblok from "@storyblok/astro";
 import { loadEnv } from 'vite';
+
 const env = loadEnv("", process.cwd(), 'STORYBLOK');
 
 
